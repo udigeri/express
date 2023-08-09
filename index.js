f515a84e02460aaf40df01e5b83c4d35a0b19f29
Udi <udigeri@gmail.com>
2023-08-09 15:19:52 +0200

    first server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Initial info Hello World");
+const http = require('http')
+
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' })
+  response.end('Hello World')
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
