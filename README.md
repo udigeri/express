8030376285d732428959845b0156ba89ddabe6ab
Udi <udigeri@gmail.com>
2023-08-09 17:33:16 +0200

    add resource for view one note

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-# Production
+# Description
+
+Web article on [fullstackopen](https://fullstackopen.com/en/part3/node_js_and_express)
+
+
+## Production
 
 `npm start`
 
-# Development
+## Development
 
 `npm run dev`
--- index.js
+++ index.js
@@ -27,6 +27,19 @@ app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
 
+app.get('/api/notes/:id', (request, response) =>{
+  const id = Number(request.params.id);
+  const note = notes.find(note => {
+    return note.id === id;
+  })
+  if (note){
+    response.json(note);
+  }
+  else{
+    response.status(404).end();
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
