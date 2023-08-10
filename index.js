45fb962825a16767de3c966d7446d634008b9a69
Udi <udigeri@gmail.com>
2023-08-10 15:04:06 +0200

    improved creation of new note

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ let notes = [
 
 app.use(express.json())
 
+app.head("/", (request, response) => {
+  response.status(301).end();
+});
+
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
@@ -42,12 +46,27 @@ app.get("/api/notes/:id", (request, response) => {
   }
 });
 
-app.post('/api/notes', (request, response) => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id)) 
+const generateId = (jsonArray) => {
+  const maxId = jsonArray.length > 0
+    ? Math.max(...jsonArray.map(n => n.id))
     : 0
-  const note = request.body
-  note.id = maxId + 1
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    id: generateId(notes),
+    content: body.content,
+    important: body.important || false,
+  }
   notes = notes.concat(note)
   response.json(note)
 })
