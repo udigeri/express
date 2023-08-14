35e1fc4557b34a3aecc4a63954276c04b5c897d5
Udi <udigeri@gmail.com>
2023-08-14 22:26:18 +0200

    MongoDB refactoring finished

--- index.js
+++ index.js
@@ -2,26 +2,9 @@ require('dotenv').config()
 const express = require('express')
 const app = express();
 const Note = require('./models/note')
-const morgan = require('morgan')
+const morgan = require('morgan');
+const note = require('./models/note');
 
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
 
 app.use(express.json());
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
@@ -43,50 +26,48 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => {
-    return note.id === id;
-  });
-  if (note) {
-    response.json(note);
-  } else {
+  Note.findById(request.params.id).then(note => {
+    if (!note){
+      response.status(400).end();
+    }
+    else{
+      response.json(note);
+    }
+  }).catch(err => {
     response.status(404).end();
-  }
+  })
 });
-
-const generateId = (jsonArray) => {
-  const maxId =
-    jsonArray.length > 0 ? Math.max(...jsonArray.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({
       error: "content missing",
     });
   }
 
-  const note = {
-    id: generateId(notes),
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-  };
-  notes = notes.concat(note);
-  response.json(note);
+  });
+
+  note.save().then(savedNote =>{
+    response.json(savedNote);
+  })
 });
 
 app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (note) {
-    notes = notes.filter((note) => note.id !== id);
-    response.status(204).end();
-  } else {
+  Note.findByIdAndDelete(request.params.id).then(note => {
+    if (!note){
+      response.status(400).end();
+    }
+    else{
+      response.status(204).end();
+    }
+  }).catch(err => {
     response.status(404).end();
-  }
+  })
 });
 
 const unknownEndpoint = (request, response) => {
