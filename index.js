da81f0d67516688f20370c531e7bbc505e7ac6a6
Udi <udigeri@gmail.com>
2023-08-14 23:56:50 +0200

    add validation

--- index.js
+++ index.js
@@ -53,6 +53,7 @@ app.post("/api/notes", (request, response) => {
   note.save().then(savedNote =>{
     response.json(savedNote);
   })
+  .catch(error => next(error))
 });
 
 app.delete("/api/notes/:id", (request, response, next) => {
@@ -67,14 +68,17 @@ app.delete("/api/notes/:id", (request, response, next) => {
 });
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+  const { content, important } = request.body
 
   const note = {
     content: body.content,
     important: body.important,
   }
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(request.params.id, 
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+    )
     .then(updatedNote => {
       if (!updatedNote){
         response.status(404).end();
@@ -99,7 +103,9 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } 
-
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
   next(error)
 }
 
