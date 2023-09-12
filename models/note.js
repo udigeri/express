29f3c08fdd1b9ae17bdca897410c45db11837103
Udi <udigeri@gmail.com>
2023-09-12 10:45:28 +0200

    cancat MongoDB url or environment variable for Windows

--- models/note.js
+++ models/note.js
@@ -1,41 +1,48 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false);
 // const password = process.argv[2]
 
 // const url =
 //   `mongodb+srv://udigeri:${password}@cluster0.z5dwrt9.mongodb.net/noteApp?retryWrites=true&w=majority`
 
-const url = process.env.MONGODB_URI
+const uri = process.env.MONGODB_URI;
+//console.log("connecting to", uri);
 
+var url = process.env.MONGODB_SCHEME;
+if (url) {
+  url += process.env.MONGODB_USER;
+  url += ":";
+  url += process.env.MONGODB_PASSWORD;
+  url += "@";
+  url += uri;
+} else url = uri;
 
-console.log('connecting to', url)
+mongoose
+  .connect(url)
 
-mongoose.connect(url)
-
-  .then(result => {
-    console.log('connected to MongoDB')
+  .then((result) => {
+    console.log("connected to MongoDB");
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
     minLength: 5,
-    required: true
+    required: true,
   },
   important: Boolean,
-})
+});
 
-noteSchema.set('toJSON', {
+noteSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-
-module.exports = mongoose.model('Note', noteSchema)
+module.exports = mongoose.model("Note", noteSchema);
